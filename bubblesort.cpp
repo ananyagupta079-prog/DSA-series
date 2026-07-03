e0ea28d7c6bba7707cc49f4a7d29aa0af674aadd
ananyagupta079-prog <ananyagupta079@gmail.com>
2026-07-03 09:08:54 +0530

    Sorting-I

---
+++ bubblesort.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+void bubble_sort(int arr [],int n){
+    for(int i=n-1;i>=0;i--){
+        int dis=0;
+        for(int j=0;j<i;j++){
+            if(arr[j]>arr[j+1]){
+                int temp=arr[j+1];
+                arr[j+1]=arr[j];
+                arr[j]=temp;
+                dis=1;
+            }
+        }
+        if(dis==0){
+        break;
+    }
+    }
+}
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    bubble_sort(arr,n);
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+}
